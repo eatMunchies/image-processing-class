3e91b76f8e1b4a84efdbe2bac371269afa96546e
Anthony Silva <anthony@Anthonys-MacBook-Air.local>
2025-09-17 12:44:50 -0700

    initial commit

---
+++ code/image_quantization/image_quantization.c
@@ -0,0 +1,8 @@
+/* 
+Author: Anthony Silva
+Assignment: Programming Assignment 1
+Class: Image Processing
+Date: 9/17/25
+File: image_quantization.c
+Description:
+*/
---
+++ code/image_sampling/image_sampling.c
@@ -0,0 +1,13 @@
+/* 
+Author: Anthony Silva
+Assignment: Programming Assignment 1
+Class: Image Processing
+Date: 9/17/25
+File: image_sampling.c
+Description:
+a program to change the special resolution of an image from 
+256x256 to 128x128, 64x64, 32x32 using image sampling by factors of 2, 4, and 8
+resize the images back to the original size 256 x 256 to see the differences
+using images lemma.png and peppers.png
+*/
+
